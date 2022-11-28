8815d678824cb037ccba3c57462ca34091303976
Fatema Bohra <fatema.bohra2003@gmail.com>
2022-11-27 20:05:50 -0400

    Add the generate.sh script file in challenge2 dir

---
+++ Lab1/challenge2/generate.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+#Making some directories and files.
+mkdir one
+cd one
+
+touch 07kindhearted 17start
+echo "people beatbox their way into my heart" >> 07kindhearted
+echo "picking flowers and sing me a song about not letting anything go, ever" >> 17start
+
+#Making subdirectories and files.
+mkdir two
+cd two
+
+touch 01never 19history
+echo "tell me not to tell me what's for dinner" >> 01never
+echo "is boring, unless it's about puppies because puppies are awesome" >> 19history
+
+mkdir three
+cd three
+
+touch 08spot 09today 10tame 18curious
+echo "the difference between these two pictures" >> 08spot
+echo "is the first day of your new life as a living thing" >> 09today
+echo "the lions before you put your head in their mouth" >> 10tame
+echo "ity killed the cat, and that's sad" >> 18curious
+#Going back to Challenge2 directory.
+cd ../../..
+
+#Making more directories and files
+mkdir this
+cd this
+
+touch 05gonna 15sulky
+echo "take you out for a steak dinner" >> 05gonna
+echo "children really test the bounds of child abuse law" >> 15sulky
+
+mkdir that
+cd that
+
+touch 06bike 11give 12expensive
+echo "all the way to town using only one leg and an expensive hat" >> 06bike
+echo "me a hug and buy me dinner because you're making me hungry" >> 11give
+echo "rings are expensive things and that's sad" >> 12expensive
+
+mkdir theother
+cd theother
+
+touch 04conscious 16you
+echo "decisions make conscious efforts" >> 04conscious
+echo "should cook dinner for the one you love, especially if it's a puppy" >> 16you
+
+#Going back to Challenge2 directory.
+cd ../../..
+
+#Making more and more directories and files.
+mkdir us
+cd us
+
+touch 02card
+echo "these kids at the door" >> 02card
+
+mkdir them
+cd them
+
+touch 03spoil 13boot 14water 20up
+echo "every puppy you see, but not the flowers" >> 03spoil
+echo "him out of the boat and save yourself" >> 13boot
+echo "the garden, unless you're scared of flowers" >> 14water
+echo "the creek without paddle will make me late for dinner" >> 20up
+
+#DONE!!!!!
+
